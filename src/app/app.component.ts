0d83736f52089872d56a9de066731113d73a9e08
Alex Owens <78552276+alexowensdev@users.noreply.github.com>
2024-08-01 21:49:36 -0600

    Finished Nav Bar spacing issues. Generated and initial setup of Hero Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
+import { HeroComponent } from "./hero/hero.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
-    NavComponent
-  ],
+    NavComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
