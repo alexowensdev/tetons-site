e1fd8c66a694a44c91652c2b046eeb8a9481c460
Alex Owens <78552276+alexowensdev@users.noreply.github.com>
2024-08-03 21:47:48 -0600

    Refactored SCSS code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { HeroComponent } from "./hero/hero.component";
+import { GalleryComponent } from "./gallery/gallery.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
-    NavComponent, HeroComponent],
+    NavComponent, HeroComponent, GalleryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
